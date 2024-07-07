4d34dc3a287719058b62db65a22f9ed380fb38e1
ThiagoFPMR <thiago.rodrigues.056@ufrn.edu.br>
2024-07-07 01:22:00 -0300

    task about processes in unix

--- main.cpp
+++ main.cpp
@@ -1,44 +1,59 @@
+#include <iostream>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
-int main() {
-    // Create a VideoCapture object to access the webcam
-    cv::VideoCapture cap(0); // 0 means the first connected webcam
+int main()
+{
+    int pid, child, status;
+    // cv::Mat Rx, Ry;
 
-    // Check if the webcam is opened successfully
-    if (!cap.isOpened()) {
-        std::cerr << "Error: Could not open the webcam." << std::endl;
-        return -1;
+    cv::Mat img = cv::imread("coins.png", cv::IMREAD_GRAYSCALE);
+
+    cv::Mat Rx = cv::Mat(img.rows, img.cols, img.type(), mmap(NULL, img.rows * img.cols * img.elemSize(), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+    cv::Mat Ry = cv::Mat(img.rows, img.cols, img.type(), mmap(NULL, img.rows * img.cols * img.elemSize(), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0));
+
+    // Definição dos operadores de Prewitt
+    cv::Mat Gx = (cv::Mat_<float>(3, 3) << -1, 0, 1,
+                  -1, 0, 1,
+                  -1, 0, 1);
+
+    cv::Mat Gy = (cv::Mat_<float>(3, 3) << -1, -1, -1,
+                  0, 0, 0,
+                  1, 1, 1);
+
+    // Processo filho de detectção de bordas no eixo x
+    pid = fork();
+    if (pid == 0)
+    {
+        cv::filter2D(img, Rx, -1, Gx);
+        exit(0);
     }
+    child = wait(&status);
 
-    // Create a window to display the video feed
-    cv::namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
-
-    while (true) {
-        cv::Mat frame;
-        
-        // Capture a new frame from the webcam
-        cap >> frame;
-
-        // Check if the frame is empty (this can happen if there's a problem with the webcam)
-        if (frame.empty()) {
-            std::cerr << "Error: Could not capture a frame." << std::endl;
-            break;
-        }
-
-        // Display the frame in the created window
-        cv::imshow("Webcam", frame);
-
-        // Wait for 30 ms and break the loop if the user presses the 'q' key
-        if (cv::waitKey(30) == 'q') {
-            break;
-        }
+    // Processo filho de detectção de bordas no eixo y
+    pid = fork();
+    if (pid == 0)
+    {
+        cv::filter2D(img, Ry, -1, Gy);
+        exit(0);
     }
+    child = wait(&status);
 
-    // Release the VideoCapture object
-    cap.release();
+    cv::Mat R = Rx + Ry;
+    cv::min(R, 255, R); // Manter os valores até 255
 
-    // Destroy all OpenCV windows
-    cv::destroyAllWindows();
+    // Mostrar a imagem final
+    cv::imshow("Filtered Image", R);
+    cv::waitKey(0);
+
+    // Liberar memória
+    munmap(Rx.data, img.rows * img.cols * img.elemSize());
+    munmap(Ry.data, img.rows * img.cols * img.elemSize());
 
     return 0;
 }
